602d4f1bf49159ee29db8622a6c72bd017084283
lucia <51058748+lucia-gomez@users.noreply.github.com>
2024-09-23 11:50:47 -0400

    add audio files + status indicator

--- script.js
+++ script.js
@@ -2,10 +2,31 @@ const audioFiles = {
   test: new Audio("test.mp3"),
 };
 
+for (let i = 1; i <= 12; i++) {
+  let n = i < 10 ? "0" + String(i) : String(i);
+  audioFiles[i] = new Audio(`Installation ${n}.mp3`);
+}
+
+const statusText = document.getElementById("status");
+let currentAudio = null;
+
 function playAudio(buttonId) {
-  const audio = audioFiles["test"]; // TODO use actual [buttonId]
+  const audio = audioFiles[buttonId];
+
+  if (currentAudio && !currentAudio.paused) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+    currentAudio.removeEventListener("ended", onAudioEnded);
+  }
+
   if (audio) {
+    statusText.textContent = `Playing ${buttonId}...`;
     audio.play();
+    currentAudio = audio;
+    currentAudio.addEventListener(
+      "ended",
+      () => (statusText.textContent = "---")
+    );
   }
 }
 
